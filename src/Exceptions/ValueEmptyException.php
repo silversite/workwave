02c6a79c8629565265ccdb88195eb5bd9fbbeba7
padalec <michal@silversite.pl>
2017-11-05 00:17:52 +0100

    add empty value exception

---
+++ src/Exceptions/ValueEmptyException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Silversite\WorWave\Exceptions;
+
+class ValueEmptyException extends \InvalidArgumentException
+{
+    public static function create(string $name): self
+    {
+        return new self(sprintf('Value of "%s" can not be empty.', $name));
+    }
+}
